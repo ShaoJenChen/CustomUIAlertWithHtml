68ffd8d43e0070506e22e74c634667a5584c36a2
shaojen <shaojen@geovision.com.tw>
2020-08-21 10:29:49 +0800

    Initial Commit

---
+++ CustomUIAlertWithHtml/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CustomUIAlertWithHtml
+//
+//  Created by ShaoJen Chen on 2020/8/21.
+//  Copyright © 2020 ShaoJen Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
